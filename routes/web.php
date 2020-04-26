a7ce59c2b0a17409882cb6dee169830c3b8e3754
zack6849 <zack@zack6849.com>
2020-04-26 18:29:45 -0400

    Managed to miss my webroutes, oops.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'GmodController@index');
+Route::get('jukebox/play', 'VideoController@play');
+Route::get('jukebox/video_title', 'VideoController@title');
